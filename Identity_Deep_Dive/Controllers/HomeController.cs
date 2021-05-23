32d6a247cebedfb467d35ee947270d9dd316a065
Ayaan Faruq <ayaan.uoda@gmail.com>
2021-05-23 23:05:36 +0600

    Add project files.

---
+++ Identity_Deep_Dive/Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+﻿using Identity_Deep_Dive.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Identity_Deep_Dive.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<PractiseUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<PractiseUser> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+
+                if (user == null)
+                {
+                    user = new PractiseUser
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        UserName = model.UserName
+                    };
+
+                    var result = await _userManager.CreateAsync(user, model.Password);
+                }
+
+                return View("Success");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+
+                if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+                {
+                    var identity = new ClaimsIdentity("cookies");
+                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                    identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+
+                    await HttpContext.SignInAsync("cookies", new ClaimsPrincipal(identity));
+
+                    return RedirectToAction("Index");
+                }
+
+                ModelState.AddModelError("", "Invalid UserName or Password");
+            }
+
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Identity_Deep_Dive/Models/PractiseUsersStore.cs
@@ -0,0 +1,153 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Identity_Deep_Dive.Models
+{
+    public class PractiseUsersStore : IUserStore<PractiseUser>,IUserPasswordStore<PractiseUser>
+    {
+
+        //===>Db Connection using Dapper
+
+        public static DbConnection GetOpenConnection()
+        {
+            var connection = new SqlConnection("Data Source= DESKTOP-RMKA9B7\\AYAAN;"+
+                                                     "database = IdentitytDeepDive;" +
+                                                     "trusted_connection = yes;" );;
+            connection.Open();
+            return connection;
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public async  Task<IdentityResult> CreateAsync(PractiseUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                await connection.ExecuteAsync(
+                     "insert into  PractiseUser([Id]," +
+                     "[UserName]," +
+                     "[NormalizedUserName]," +
+                     "[PasswordHash]) " +
+                     "Values(@id,@userName,@normalizedUserName,@passwordHash)",
+                     new
+                     {
+                         id = user.Id,
+                         userName = user.UserName,
+                         normalizedUserName = user.NormalizedUserName,
+                         passwordHash = user.PasswordHash
+                     }
+                 );
+
+            }
+                return IdentityResult.Success;
+            
+
+
+        }
+
+        public async Task<IdentityResult> UpdateAsync(PractiseUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                await connection.ExecuteAsync(
+                    "update PractiseUser " +
+                    "set [Id] = @id," +
+                    "[UserName] = @userName," +
+                    "[NormalizedUserName] = @normalizedUserName," +
+                    "[PasswordHash] = @passwordHash " +
+                    "where [Id] = @id",
+                    new
+                    {
+                        id = user.Id,
+                        userName = user.UserName,
+                        normalizedUserName = user.NormalizedUserName,
+                        passwordHash = user.PasswordHash
+                    }
+                );
+            }
+
+            return IdentityResult.Success;
+        }
+
+
+        public Task<IdentityResult> DeleteAsync(PractiseUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+           
+
+        public async Task<PractiseUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                return await connection.QueryFirstOrDefaultAsync<PractiseUser>(
+                    "select * From PractiseUser where Id = @id",
+                    new { id = userId });
+            }
+        }
+
+        public async Task<PractiseUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                return await connection.QueryFirstOrDefaultAsync<PractiseUser>(
+                    "select * From PractiseUser where NormalizedUserName = @name",
+                    new { name = normalizedUserName });
+            }
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(PractiseUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.NormalizedUserName);
+        }
+
+        public Task<string> GetUserIdAsync(PractiseUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.Id);
+        }
+
+        public Task<string> GetUserNameAsync(PractiseUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.UserName);
+        }
+
+        public Task SetNormalizedUserNameAsync(PractiseUser user, string normalizedUserName, CancellationToken cancellationToken)
+        {
+            user.NormalizedUserName = normalizedUserName;
+            return Task.CompletedTask;
+        }
+
+        public Task SetUserNameAsync(PractiseUser user, string userName, CancellationToken cancellationToken)
+        {
+            user.UserName = userName;
+            return Task.CompletedTask;
+
+        }
+
+        public Task SetPasswordHashAsync(PractiseUser user, string passwordHash, CancellationToken cancellationToken)
+        {
+            user.PasswordHash = passwordHash;
+            return Task.CompletedTask;
+        }
+
+        public Task<string> GetPasswordHashAsync(PractiseUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        public Task<bool> HasPasswordAsync(PractiseUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PasswordHash != null);
+        }
+    }
+}
